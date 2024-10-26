d7c441c8b4edec1a0f128ce5e4cfe857a46d0e4b
Piotr Synowiec <mysiar@users.noreply.github.com>
2024-10-26 10:50:18 +0200

    Streamlit Cloud deployment changes

--- setup.py
+++ setup.py
@@ -16,6 +16,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mysiar-org/st-table",
+    project_urls={
+        "Documentation": "https://github.com/mysiar-org/st-table",
+        "Changelog": "https://github.com/mysiar-org/st-table/blob/master/CHANGELOG.md",
+        "Issues": "https://github.com/mysiar-org/st-table/issues",
+        "Examples": "https://mysiar-org-st-table-st-df.streamlit.app",
+    },
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
